400e19a9001997cdce321858423fbac48648086b
LazarJovic <lazar.13jovic@gmail.com>
2020-12-20 12:25:50 +0100

    Set up info api for Bitcoin-Payment-Service

---
+++ Bitcoin-Payment-Service/src/main/java/goveed20/BitcoinPaymentService/controllers/PageController.java
@@ -0,0 +1,14 @@
+package goveed20.BitcoinPaymentService.controllers;
+
+import goveed20.PaymentConcentrator.payment.concentrator.plugin.PluginPageController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PageController implements PluginPageController {
+
+    @Override
+    public String getPluginPage(String id) {
+        return null;
+    }
+
+}
