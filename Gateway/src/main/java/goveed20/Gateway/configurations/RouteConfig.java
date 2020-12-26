dbe5d3d8515ea2deac6f50b56a58f82a26dd8bde
Dragan Ćulibrk <draganculibrk9@gmail.com>
2020-12-26 15:38:03 +0100

    Remove Zipkin and Sleuth

--- Gateway/src/main/java/goveed20/Gateway/configurations/RouteConfig.java
+++ Gateway/src/main/java/goveed20/Gateway/configurations/RouteConfig.java
@@ -1,6 +1,5 @@
 package goveed20.Gateway.configurations;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
