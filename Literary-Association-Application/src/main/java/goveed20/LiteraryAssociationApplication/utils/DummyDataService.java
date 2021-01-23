1fdbf182c1ddf5d20ebc7f6777b71dfa72a54bca
Dragan Ćulibrk <draganculibrk9@gmail.com>
2021-01-23 07:43:45 +0100

    Create dummy board members

---
+++ Literary-Association-Application/src/main/java/goveed20/LiteraryAssociationApplication/utils/DummyDataService.java
@@ -0,0 +1,52 @@
+package goveed20.LiteraryAssociationApplication.utils;
+
+import goveed20.LiteraryAssociationApplication.model.BaseUser;
+import goveed20.LiteraryAssociationApplication.model.Location;
+import goveed20.LiteraryAssociationApplication.model.enums.UserRole;
+import goveed20.LiteraryAssociationApplication.repositories.BaseUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DummyDataService {
+    @Autowired
+    private BaseUserRepository baseUserRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void addDummyBoardMembers() {
+        if (baseUserRepository.findAllByRole(UserRole.BOARD_MEMBER).isEmpty()) {
+            BaseUser boardMember1 = BaseUser.builder()
+                    .name("board_member_1_name")
+                    .surname("board_member_1_surname")
+                    .email("board_member_1@test.com")
+                    .username("board_member_1")
+                    .password(passwordEncoder.encode("board_member_1"))
+                    .verified(true)
+                    .role(UserRole.BOARD_MEMBER)
+                    .location(Location.builder().longitude(0.0).latitude(0.0).city("test1").country("test1").build())
+                    .build();
+
+            BaseUser boardMember2 = BaseUser.builder()
+                    .name("board_member_2_name")
+                    .surname("board_member_2_surname")
+                    .email("board_member_2@test.com")
+                    .username("board_member_2")
+                    .verified(true)
+                    .role(UserRole.BOARD_MEMBER)
+                    .password(passwordEncoder.encode("board_member_2"))
+                    .location(Location.builder().longitude(0.0).latitude(0.0).city("test2").country("test2").build())
+                    .build();
+
+            baseUserRepository.save(boardMember1);
+            baseUserRepository.save(boardMember2);
+
+            System.out.println("Created dummy board members!");
+        }
+    }
+}
