4cc9d85ca0794695b88dfd8102f94fa0b2168429
Danijel Radulović <master.daca09@gmail.com>
2021-02-04 15:30:45 +0100

    Update Book.jsx

--- literary-association-frontend/src/components/Book.jsx
+++ literary-association-frontend/src/components/Book.jsx
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button, Col, Row, Spinner } from 'react-bootstrap'
 import { setBook } from '../reducers/bookReducer'
+import bookService from '../services/bookService'
+import PaymentModal from './PaymentModal'
+import CurrencyFormat from 'react-currency-format'
 
 const Book = () => {
     const dispatch = useDispatch()
     const book = useSelector(state => state.books.shown)
+    const [modalShown, setModalShown] = useState(false)
 
     const idMatch = useRouteMatch('/dashboard/books/:id')
 
     useEffect(() => {
-        if (!book) {
+        if (!book || book.id !== idMatch) {
             idMatch && dispatch(setBook(idMatch.params.id))
         }
     }, [])
@@ -24,28 +28,41 @@ const Book = () => {
         )
     }
 
+    const buttonName = book.price === 0 ? 'Download' : 'Purchase'
+    const toggleModal = () => setModalShown(!modalShown)
+    const onClick = () => book.price === 0 ? bookService.downloadBook(book.title, null) : toggleModal()
+
     return (
-        <Row style={{ width: '50%', marginTop: '2%', marginLeft: 'auto', marginRight: 'auto' }}>
-            <Col>
-                <Row>Title:</Row>
-                <Row>ISBN:</Row>
-                <Row>Genre:</Row>
-                <Row>Publisher:</Row>
-                <Row>City:</Row>
-                <Row>Year:</Row>
-                <Row>Synopsis:</Row>
-            </Col>
-            <Col>
-                <Row><b>{book.title}</b></Row>
-                <Row>{book.isbn}</Row>
-                <Row>{book.genreEnum}</Row>
-                <Row>{book.publisher}</Row>
-                <Row>{book.place}</Row>
-                <Row>{book.year}</Row>
-                <Row><i>{book.synopsis}</i></Row>
-                <Row><Button type='button'>{book.price === 0 ? 'Download' : 'Purchase'}</Button></Row>
-            </Col>
-        </Row>
+        <>
+            <PaymentModal show={modalShown} toggleModal={toggleModal}/>
+            <Row style={{ width: '60%', marginTop: '2%', marginLeft: 'auto', marginRight: 'auto' }}>
+                <Col style={{ borderRight: '1px solid #333' }}>
+                    <Row>Title:</Row>
+                    <Row>ISBN:</Row>
+                    <Row>Genre:</Row>
+                    <Row>Publisher:</Row>
+                    <Row>City:</Row>
+                    <Row>Year:</Row>
+                    <Row>Synopsis:</Row>
+                    <Row>Price:</Row>
+                </Col>
+                <Col/>
+                <Col style={{ whiteSpace: 'nowrap' }}>
+                    <Row><b>{book.title}</b></Row>
+                    <Row>{book.isbn}</Row>
+                    <Row>{book.genreEnum}</Row>
+                    <Row>{book.publisher}</Row>
+                    <Row>{book.place}</Row>
+                    <Row>{book.year}</Row>
+                    <Row><i>{book.synopsis}</i></Row>
+                    <Row>
+                        <CurrencyFormat value={book.price} displayType={'text'} thousandSeparator={true} prefix={'$'}
+                                        renderText={value => <p>{value}</p>}/>
+                    </Row>
+                    <Row><Button onClick={onClick} type='button'>{buttonName}</Button></Row>
+                </Col>
+            </Row>
+        </>
     )
 }
 
